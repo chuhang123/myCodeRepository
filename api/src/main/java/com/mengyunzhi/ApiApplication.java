6bee996227ea99c91ae68420ec5caddc25956655
chuhang123 <chuhang199708>
2017-11-25 22:33:24 +0800

    引入相关依赖

---
+++ api/src/main/java/com/mengyunzhi/ApiApplication.java
@@ -0,0 +1,15 @@
+package com.mengyunzhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by chuhang on 17-11-25
+ */
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
